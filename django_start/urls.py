8c43b173912f5e861ed7b9fbae78c8985661398a
Arjun Balamwar <arjunbalamwar@gmail.com>
2020-09-26 00:08:02 +0530

    https://github.com/Adarsh1011/faulty-

--- django_start/urls.py
+++ django_start/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('register/', user_views.register, name="register"),
     path('profile/', user_views.profile, name="profile"),
     path('predict/', predict_views.predict, name="predict"),
-    # path('predictMPG/', predict_views.predictMPG, name="predictMPG"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('password-reset/', 
